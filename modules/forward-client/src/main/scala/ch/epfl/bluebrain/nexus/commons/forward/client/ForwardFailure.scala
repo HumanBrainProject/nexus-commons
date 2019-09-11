c6f9ba0f8599ca4208587c62558ca33836eca803
David Kunzmann <davidkunzmann@gmail.com>
2019-09-11 08:42:27 +0200

    Added more logs

--- modules/forward-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/forward/client/ForwardFailure.scala
+++ modules/forward-client/src/main/scala/ch/epfl/bluebrain/nexus/commons/forward/client/ForwardFailure.scala
@@ -6,6 +6,7 @@ import cats.MonadError
 import cats.syntax.functor._
 import ch.epfl.bluebrain.nexus.commons.http.HttpClient.UntypedHttpClient
 import ch.epfl.bluebrain.nexus.commons.types.{Err, RetriableErr}
+import journal.Logger
 
 @SuppressWarnings(Array("IncorrectlyNamedExceptions"))
 trait ForwardFailure extends Err {
@@ -18,16 +19,19 @@ trait ForwardFailure extends Err {
 
 @SuppressWarnings(Array("IncorrectlyNamedExceptions"))
 object ForwardFailure {
+  private[client] val log = Logger[this.type]
 
   /**
     * Generates a Forward failure from the HTTP response .
     *
     * @param r the HTTP response
     */
-  def fromResponse[F[_]](r: HttpResponse)(implicit cl: UntypedHttpClient[F],
-                                          F: MonadError[F, Throwable]): F[ForwardFailure] =
+  def fromResponse[F[_]](
+    r: HttpResponse
+  )(implicit cl: UntypedHttpClient[F], F: MonadError[F, Throwable]): F[ForwardFailure] = {
+    log.info(s"Forward failure response info - ${r.toString()}")
     cl.toString(r.entity).map(body => fromStatusCode(r.status, body))
-
+  }
 
   /**
     * Generates a Forward server failure from the HTTP response status ''code''.
@@ -49,7 +53,7 @@ object ForwardFailure {
     * @param body   the response body returned by the Forward endpoint
     */
   final case class ForwardServerError(status: StatusCode, body: String)
-    extends RetriableErr(s"Server error with status code '$status'")
+      extends RetriableErr(s"Server error with status code '$status'")
       with ForwardFailure
 
   /**
@@ -59,7 +63,7 @@ object ForwardFailure {
     * @param body   the response body returned by the Forward endpoint
     */
   final case class ForwardClientError(status: StatusCode, body: String)
-    extends Err(s"Client error with status code '$status'")
+      extends Err(s"Client error with status code '$status'")
       with ForwardFailure
 
   /**
@@ -69,7 +73,7 @@ object ForwardFailure {
     * @param body   the response body returned by the Forward endpoint
     */
   final case class ForwardUnexpectedError(status: StatusCode, body: String)
-    extends RetriableErr(s"Unexpected error with status code '$status'")
+      extends RetriableErr(s"Unexpected error with status code '$status'")
       with ForwardFailure
 
   /**
@@ -79,6 +83,6 @@ object ForwardFailure {
     * @param body   the response body returned by the Forward endpoint
     */
   final case class ForwardError(status: StatusCode, body: String)
-    extends RetriableErr(s"Unexpected error with status code '$status'")
+      extends RetriableErr(s"Unexpected error with status code '$status'")
       with ForwardFailure
 }
